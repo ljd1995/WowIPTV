1a489f22fdfefc239ba6828fe477b381124b527b
jack <jackless1995@outlook.com>
2026-07-31 19:49:56 +0700

    feat: appcompat 语言切换基础设施(AppCompatActivity + localeConfig)

--- app/src/main/java/com/dream/wowiptv/MainActivity.kt
+++ app/src/main/java/com/dream/wowiptv/MainActivity.kt
@@ -3,9 +3,9 @@ package com.dream.wowiptv
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.compose.rememberNavController
@@ -14,7 +14,7 @@ import com.dream.wowiptv.presentation.navigation.AppNavGraph
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
