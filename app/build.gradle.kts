bf77a42598c551f5250df2ad38d8fb67577c5eb5
jack <jackless1995@outlook.com>
2026-08-02 18:13:34 +0700

    release: 版本改为1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = providers.gradleProperty("versionName").orNull ?: "1.4"
+        versionName = providers.gradleProperty("versionName").orNull ?: "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
